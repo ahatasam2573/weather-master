a9d5e1d1d0d044c636d1399d0b4322c4b9e241e8
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-06-15 10:36:57 +0600

    components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButton from './components/TopButton';
+import Inputs from './components/Inputs'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl text-blue-500 font-bold'>Hello welcome to my weather app</h1>
-      <UilReact size={50} />
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButton />
+      <Inputs />
     </div>
   );
-}
+} g
 
 export default App;
