e084933865789c10b3fc574f35112d48567dfc86
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-06-15 10:07:08 +0600

    unicons installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-2xl text-blue-500 font-bold'>Hello welcome to my weather app</h1>
+      <UilReact size={50} />
     </div>
   );
 }
