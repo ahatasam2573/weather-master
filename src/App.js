0ca0b3b758216759a04d63da8f556ad27acfd089
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-06-21 16:38:58 +0600

    Time and location component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import TopButton from './components/TopButton';
 import Inputs from './components/Inputs'
-import TimeAndLLocation from './components/TimeAndLLocation';
+import TimeAndLocation from './components/TimeAndLocation';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButton />
       <Inputs />
-      <TimeAndLLocation />
+      <TimeAndLocation />
     </div>
   );
 };
---
+++ src/components/TimeAndLocation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TimeAndLocation = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center my-6'>
+                <p className='text-white text-xl font-extraLight'>
+                    Monday, 13 December 2022 | Local time: 1:05 PM
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default TimeAndLocation;
